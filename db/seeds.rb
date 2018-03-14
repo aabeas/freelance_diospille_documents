08e143e819a0cc0c0fae1bc56aeaaa29d6157ba3
aabeas <aaron.beasley@gmail.com>
2018-03-14 01:12:31 -0500

    Create seed file and integrate cors gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |d|
+  FreelanceDocument.create!(
+    title: "Document #{d}",
+    description: "Aliquam erat volutpat. Nullam euismod mattis nisl sit amet eleifend. Pellentesque vel fringilla lacus. In hac habitasse platea dictumst.",
+    file_url: "https://docs.google.com/document/d/1GrlrwJy_aHw78M3mJ9davPTci88YPq1THJJ1tWQ0ZC4/edit?usp=sharing",
+    image_url: "https://placeholdit.co//i/318x180?bg=0d48c4"
+  )
+end
